e10efb51ffb2a5e8823d9bf54b19bd4f9400c00d
dcooper1109 <dan.cooper@exouza.com>
2026-07-31 09:42:39 -0400

    Metlife FAQ

--- app/faq/data/index.ts
+++ app/faq/data/index.ts
@@ -1,6 +1,7 @@
 import type { FaqItem } from "./types";
 import { fairshareFaqs } from "./fairshare";
 import { testclientFaqs } from "./testclient";
+import { metlifeFaqs } from "./metlife";
 
 export const partnerFaqs: Record<string, FaqItem[]> = {
   fairshare: fairshareFaqs,
