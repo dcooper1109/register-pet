ba79be62bc066f3500af2f4863c5bf5a0745238c
dcooper1109 <dan.cooper@exouza.com>
2026-08-07 19:56:48 -0400

    change autho.ts 3

--- app/api/stripe/create-checkout-session/route.ts
+++ app/api/stripe/create-checkout-session/route.ts
@@ -313,7 +313,7 @@ export async function POST(request: Request) {
       },
 
       success_url:
-        "https://purchase.petvantagerx.com/" +
+        "https://login.petvantagerx.com/" +
         "?stripe_session_id={CHECKOUT_SESSION_ID}",
 
       cancel_url:
